637ffab453f8523dd7624c848e08e93cd94122d3
patelDhyey56 <patekdhyey200@gmail.com>
2023-07-17 15:04:03 +0530

    adding rechecking password logic

--- BACK_END/controller/Signupcontroller.js
+++ BACK_END/controller/Signupcontroller.js
@@ -1,55 +1,59 @@
 import Customer from '../Schema/Signupschema.js'
 import jwt from "jsonwebtoken"
 
-export const getsignup =async(req,res)=>{
+export const getsignup = async (req, res) => {
     const register = await Customer.find({})
-    res.json({data:register})
+    res.json({ data: register })
 }
-export const postlogin =async(req,res)=>{
-    const { email , password} =req.body
+export const postlogin = async (req, res) => {
+    const { email, password } = req.body
     const register = await Customer.findOne({ email })
-    if(!register){
-        res.status(406).json({message:"User not exiest..."})
+    if (!register) {
+        res.status(406).json({ message: "User not exiest..." })
     }
     // console.log(register.password)
     // const checkpass =register.password
     // if(!(checkpass === password)){
     //     res.status(406).json({message:"User exiest but password not matched..."})
     // }
-    else{
-
-        const payload ={
-            name:email,
+    else {
+        const checkPass = register.password;
+        if (!(checkPass === password)) {
+            res.status(406).json({ message: "Invalid password..." });   
         }
-        const token =jwt.sign(payload,'secret')
-        // console.log(token)
-        res.json({message:"user exiest" , token})
+        else {
+            const payload = {
+                name: email,
+            };
+            const token = jwt.sign(payload, 'secret');
+            res.json({ message: "User exists", token });
+        }
     }
 }
-export const postsignup = async (req,res)=>{
-    const { fname , lname , email , password , repassword} =req.body
-    if(password===repassword){
+export const postsignup = async (req, res) => {
+    const { fname, lname, email, password, repassword } = req.body
+    if (password === repassword) {
         const customer = new Customer({
-            email:email,
-            fname:fname,
-            lname:lname,
-            password:password,
-            repassword:repassword,
+            email: email,
+            fname: fname,
+            lname: lname,
+            password: password,
+            repassword: repassword,
         })
         await customer.save()
-        res.json({message:"sucess...."})
+        res.json({ message: "sucess...." })
     }
-    else{
-        res.json({message:"invalid details....."})
+    else {
+        res.json({ message: "invalid details....." })
     }
 }
-export const deletesignup =async(req,res)=>{
+export const deletesignup = async (req, res) => {
     // console.log(req.params)
-    await Customer.deleteOne({_id:req.params.id})
-    res.json({message:"sucess...."})
+    await Customer.deleteOne({ _id: req.params.id })
+    res.json({ message: "sucess...." })
 }
-export const patchsignup = async(req,res)=>{
+export const patchsignup = async (req, res) => {
     // console.log(req.params)
-    await Customer.updateOne({_id : req.params.id},{$set : req.body})
-    res.json({message:"sucess...."})
+    await Customer.updateOne({ _id: req.params.id }, { $set: req.body })
+    res.json({ message: "sucess...." })
 }
