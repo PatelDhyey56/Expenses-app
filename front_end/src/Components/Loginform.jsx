f170a3a6fe11116d35ec87e8637889fc62ff7617
patelDhyey56 <patekdhyey200@gmail.com>
2023-07-19 14:56:45 +0530

    edit login page

--- front_end/src/Components/Loginform.jsx
+++ front_end/src/Components/Loginform.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { useState } from 'react'
 import Cookies from "js-cookie"
-import {useNavigate} from "react-router-dom"
+import { useNavigate } from "react-router-dom"
 
 export default function Loginform() {
-    const navigate =useNavigate()
+    const navigate = useNavigate()
     const initialform =
     {
         email: '',
@@ -17,7 +17,7 @@ export default function Loginform() {
             ...loginform, [e.target.name]: e.target.value
         })
     }
-    const submitform = async(e) => {
+    const submitform = async (e) => {
         e.preventDefault()
         const res = await fetch("http://localhost:8000/signup/login", {
             method: "POST",
@@ -26,13 +26,13 @@ export default function Loginform() {
                 'content-type': 'application/json',
             }
         })
-        const {token} =await res.json()
-        if(res.ok){
-            Cookies.set('token',token)
-            console.log("successfully login...")
+        const { token } = await res.json()
+        if (res.ok) {
             navigate('/')
+            Cookies.set('token', token)
+            // console.log("successfully login...")
         }
-        else{
+        else {
             console.log("invalid......")
             window.alert("invalid details...")
         }
@@ -46,39 +46,47 @@ export default function Loginform() {
                         <div className='m-5'>
                             <div className='m-5'>
                                 <div className='m-5'>
-                                    <div className='m-5'>
-                                        <form onSubmit={submitform}>
-                                            <div className="mb-3">
-                                            </div>
-                                            <div className="mb-3">
-                                                <input
-                                                    type="email"
-                                                    name='email'
-                                                    value={loginform.email}
-                                                    onChange={changelogiform}
-                                                    placeholder="Email"
-                                                    id="exampleInputEmail1"
-                                                    className="form-control"
-                                                    aria-describedby="emailHelp"
-                                                    required
-                                                />
-                                            </div>
-                                            <div className="mb-3">
-                                                <input
-                                                    type="password"
-                                                    name='password'
-                                                    value={loginform.password}
-                                                    onChange={changelogiform}
-                                                    placeholder="Password"
-                                                    className="form-control"
-                                                    id="exampleInputPassword1"
-                                                    required
-                                                />
-                                            </div>
-                                            <button type="submit" className="btn btn-primary">
-                                                Submit
-                                            </button>
-                                        </form>
+                                    <div className='card m-5'>
+                                        <div className="text-center">
+                                            <form >
+                                                <div class="text-center">
+                                                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk6qKlLk50SrpDFulviyCpf0E2AcU9ncWXpA&usqp=CAU" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                                                        width="200px" alt="profile" />
+                                                </div>
+                                                <div className="mb-3">
+                                                </div>
+                                                <div className="m-3">
+                                                    <input
+                                                        type="email"
+                                                        name='email'
+                                                        value={loginform.email}
+                                                        onChange={changelogiform}
+                                                        placeholder="Email"
+                                                        id="exampleInputEmail1"
+                                                        className="form-control"
+                                                        aria-describedby="emailHelp"
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="m-3">
+                                                    <input
+                                                        type="password"
+                                                        name='password'
+                                                        value={loginform.password}
+                                                        onChange={changelogiform}
+                                                        placeholder="Password"
+                                                        className="form-control"
+                                                        id="exampleInputPassword1"
+                                                        required
+                                                    />
+                                                </div>
+                                                <div className="m-3">
+                                                    <button type="submit" className="btn btn-primary" onClick={submitform}>
+                                                        Submit
+                                                    </button>
+                                                </div>
+                                            </form>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
--- front_end/src/Components/Signupform.jsx
+++ front_end/src/Components/Signupform.jsx
@@ -49,7 +49,7 @@ export default function Signupform({ editcus, fetchdata, setEditcus }) {
             console.log(data);
             reload(res)
         }
-        else{
+        else {
             window.alert("plese enter valid details......")
             setForm(initialform)
         }
@@ -75,88 +75,98 @@ export default function Signupform({ editcus, fetchdata, setEditcus }) {
                     <div className='m-5'>
                         <div className='m-5'>
                             <div className='m-5'>
-                                <div className='m-5'>
-                                    <form onSubmit={submitform}>
-                                        <div className="mb-3">
-                                            <div className="row">
-                                                <div className="col">
-                                                    <input
-                                                        type="fname"
-                                                        name='fname'
-                                                        value={form.fname}
-                                                        onChange={changeform}
-                                                        id="exampleInputfname"
-                                                        className="form-control"
-                                                        placeholder="First name"
-                                                        aria-label="First name"
-                                                        required
-                                                    />
-                                                </div>
-                                                <div className="col">
-                                                    <input
-                                                        type="lname"
-                                                        name='lname'
-                                                        value={form.lname}
-                                                        onChange={changeform}
-                                                        id="exampleInputlname"
-                                                        className="form-control"
-                                                        placeholder="Last name"
-                                                        aria-label="Last name"
-                                                        required
-                                                    />
+                                <div className='card m-5'>
+                                    <div className="text-center">
+                                        <form onSubmit={submitform}>
+                                            <div class="text-center">
+                                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSk6qKlLk50SrpDFulviyCpf0E2AcU9ncWXpA&usqp=CAU" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                                                    width="200px" alt="profile" />
+                                            </div>
+                                            <div className="m-3">
+                                                <div className="row">
+                                                    <div className="col">
+                                                        <input
+                                                            type="fname"
+                                                            name='fname'
+                                                            value={form.fname}
+                                                            onChange={changeform}
+                                                            id="exampleInputfname"
+                                                            className="form-control"
+                                                            placeholder="First name"
+                                                            aria-label="First name"
+                                                            required
+                                                        />
+                                                    </div>
+                                                    <div className="col">
+                                                        <input
+                                                            type="lname"
+                                                            name='lname'
+                                                            value={form.lname}
+                                                            onChange={changeform}
+                                                            id="exampleInputlname"
+                                                            className="form-control"
+                                                            placeholder="Last name"
+                                                            aria-label="Last name"
+                                                            required
+                                                        />
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                        <div className="mb-3">
-                                            <input
-                                                type="email"
-                                                name='email'
-                                                value={form.email}
-                                                onChange={changeform}
-                                                placeholder="Email"
-                                                id="exampleInputEmail1"
-                                                className="form-control"
-                                                aria-describedby="emailHelp"
-                                                required
-                                            />
-                                        </div>
-                                        <div className="mb-3">
-                                            <input
-                                                type="password"
-                                                name='password'
-                                                value={form.password}
-                                                onChange={changeform}
-                                                placeholder="Password"
-                                                className="form-control"
-                                                id="exampleInputPassword1"
-                                                required
-                                            />
-                                        </div>
-                                        <div className="mb-3">
-                                            <input
-                                                type="password"
-                                                name='repassword'
-                                                value={form.repassword}
-                                                onChange={changeform}
-                                                placeholder="Repassword"
-                                                className="form-control"
-                                                id="exampleInputrePassword1"
-                                                required
-                                            />
-                                        </div>
-                                        {
-                                            editcus.fname !== undefined &&
-                                            <button type="submit" className="btn btn-primary">
-                                                Update
-                                            </button>
-                                        }
-                                        {
-                                            editcus.fname === undefined &&
-                                            <button type="submit" className="btn btn-primary">
-                                                Submit
-                                            </button>
-                                        }
-                                    </form>
+                                            <div className="m-3">
+                                                <input
+                                                    type="email"
+                                                    name='email'
+                                                    value={form.email}
+                                                    onChange={changeform}
+                                                    placeholder="Email"
+                                                    id="exampleInputEmail1"
+                                                    className="form-control"
+                                                    aria-describedby="emailHelp"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="m-3">
+                                                <input
+                                                    type="password"
+                                                    name='password'
+                                                    value={form.password}
+                                                    onChange={changeform}
+                                                    placeholder="Password"
+                                                    className="form-control"
+                                                    id="exampleInputPassword1"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="m-3">
+                                                <input
+                                                    type="password"
+                                                    name='repassword'
+                                                    value={form.repassword}
+                                                    onChange={changeform}
+                                                    placeholder="Repassword"
+                                                    className="form-control"
+                                                    id="exampleInputrePassword1"
+                                                    required
+                                                />
+                                            </div>
+                                            {
+                                                editcus.fname !== undefined &&
+                                                <div className="m-3">
+                                                    <button type="submit" className="btn btn-primary">
+                                                        Update
+                                                    </button>
+                                                </div>
+                                            }
+                                            {
+                                                editcus.fname === undefined &&
+                                                <div className="m-3">
+                                                    <button type="submit" className="btn btn-primary">
+                                                        Submit
+                                                    </button>
+                                                </div>
+                                            }
+                                        </form>
+                                    </div>
                                 </div>
                             </div>
                         </div>
